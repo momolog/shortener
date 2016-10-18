f8df26f33cb036af2d04d5bed71ac80749c2bd4b
Alexander Presber <post@momolog.info>
2016-10-18 23:00:57 +0200

    debug output

--- app/controllers/shortener/shortened_urls_controller.rb
+++ app/controllers/shortener/shortened_urls_controller.rb
@@ -1,6 +1,7 @@
 class Shortener::ShortenedUrlsController < ActionController::Base
 
   def show
+    Rails.logger.warn ">>>> show"
     token = ::Shortener::ShortenedUrl.extract_token(params[:id])
     Rails.logger.warn token
     url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: params)
