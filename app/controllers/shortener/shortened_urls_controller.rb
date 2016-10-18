11cbca2e13fcf9eb54665ed971a27325caa9386e
Alexander Presber <post@momolog.info>
2016-10-18 22:55:51 +0200

    debug

--- app/controllers/shortener/shortened_urls_controller.rb
+++ app/controllers/shortener/shortened_urls_controller.rb
@@ -2,7 +2,9 @@ class Shortener::ShortenedUrlsController < ActionController::Base
 
   def show
     token = ::Shortener::ShortenedUrl.extract_token(params[:id])
+    Rails.logger.warn token
     url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: params)
+    Rails.logger.warn url
     redirect_to url[:url], status: :moved_permanently
   end
 
